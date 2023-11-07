e5d13f92e998249c395c19bfb24cb80994efebc8
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-07 15:42:10 +0100

    completed main exercise

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@ const { createApp } = Vue
     //* METHODS
     methods : {
         getEmail(){
-            //* axios
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=> {
-                console.log(email.data.response);
-            })
+            // cycle 12 random emials
+            for (let i = 0; i < 12; i++) {
+                //* axios
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=> {
+                    // push emial to list
+                    this.emailList.push(email.data.response)      
+                })
+            }
         }
     },
     //* HOOKS
     mounted(){
-        this.getEmail()
     }
   }).mount('#app')
