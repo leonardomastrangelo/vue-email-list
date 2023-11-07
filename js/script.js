b0f333deac5f7f8170886f8db0959508e1eb3443
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-07 17:49:01 +0100

    added image api

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    //* DATA
-    data() {
-      return {
-        emailList : [],
-        max : 12
+createApp({
+  //* DATA
+  data() {
+    return {
+      emailList: [],
+      max: 12,
+      photos: [],
+      activeIndex: 0
+    };
+  },
+  //* METHODS
+  methods: {
+    getEmail() {
+      // refresh
+      this.emailList = [];
+      // cycle 12 random emials
+      for (let i = 0; i < this.max; i++) {
+        //* axios
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((email) => {
+            // push emial to list
+            this.emailList.push(email.data.response);
+          });
       }
     },
-    //* METHODS
-    methods : {
-        getEmail(){
-            // refresh
-            this.emailList = []
-            // cycle 12 random emials
-            for (let i = 0; i < this.max; i++) {
-                //* axios
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=> {
-                    // push emial to list
-                    this.emailList.push(email.data.response)      
-                })
-            }
+    getPhotos() {
+        axios.get("https://api.pexels.com/v1/search?query=people", {
+        headers: {
+            authorization: 'arjVaGJ7Oe6B1aZNeDtXPjq5NQdC4tZvyRwDteSytoCwfe6qq3rSdOUJ'
         }
-    },
-    //* HOOKS
-    mounted(){
+    }).then((prova) => {
+        // refresh
+        this.photos = [];
+        for (let i = 0; i < this.max; i++) {
+            this.photos.push(prova.data.photos[i].src.large)
+        }
+        });
     }
-  }).mount('#app')
+  },
+  //* HOOKS
+  mounted() {
+  },
+}).mount("#app");
+
+// arjVaGJ7Oe6B1aZNeDtXPjq5NQdC4tZvyRwDteSytoCwfe6qq3rSdOUJ
